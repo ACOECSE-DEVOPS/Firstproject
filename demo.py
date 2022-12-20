efb6c2282897408977f46006375ce33419a92ba3
ACOE DEVOPS <acoecolledecse@gmail.com>
2022-12-20 22:47:21 +0530

    third

--- demo.py
+++ demo.py
@@ -1,2 +1,12 @@
 print("HI THIS IS DURGA PRASAD")
 print("THIS IS SIRISHA")
+from flask import Flask,render_template
+app = Flask(__name__)
+
+@app.route('/')
+def demo():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run();
